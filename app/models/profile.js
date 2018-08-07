a0082950a04fb256a67240feb1be84d08b3cb4cc
filip <zwolifil>
2018-08-07 17:11:29 +0200

    feat(): add private option on avatar image

--- app/models/profile.js
+++ app/models/profile.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose.model('Profile', {
-    avatar : {type : String, default: ''},
+    avatar : {type : {image: String, ifPrivate: Boolean}, default: ''},
     description: {type: String, default: ''},
     name: {type: String, default: ''},
     _id: {type: String, default: ''},
