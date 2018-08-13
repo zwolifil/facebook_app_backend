bee1930c013ae71cedd1ad3660ea7e0a0fff8374
filip <zwolifil>
2018-08-13 18:14:02 +0200

    feat(): add delete request on comments

--- app/routes/comments.js
+++ app/routes/comments.js
@@ -1,9 +1,15 @@
-const multer = require('multer');
 const Comment = require('../models/comment');
 
 module.exports = function(app) {
     app.get('/comments', function(req, res) {
         Comment.find({},function(err,data){
+            if(err) throw err;
+            res.send(data);
+        });
+    });
+
+    app.delete('/comments/:id', function(req, res) {
+        Comment.remove({_id: req.params['id']},function(err,data){
             if(err) throw err;
             res.send(data);
         });
